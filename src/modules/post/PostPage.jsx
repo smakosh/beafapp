a2406c92b968ace8f2d37934a865f4072507e42a
smakosh <ismai23l@hotmail.com>
2019-01-18 05:44:56 +0100

    fix mobile view post

--- src/modules/post/PostPage.jsx
+++ src/modules/post/PostPage.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getPostById, voteBefore, voteAfter, postNewComment, deleteComment, deletePost } from './actions'
 import { Loading, SEO, Post, Container, Button } from '../../components/common'
-import { Wrapper, Overlay } from './styles'
+import { Wrapper, Overlay, Left } from './styles'
+import backArrow from './assets/back.svg'
 
 const PostPage = ({
 	auth,
@@ -15,40 +16,42 @@ const PostPage = ({
 	deleteComment,
 	deletePost,
 	history
-}) => {
-	console.log(history)
-	return (
-		<Wrapper>
-			<SEO
-				url={`/post/${singlePost.post._id}`}
-				title={singlePost.post.title}
-				description={singlePost.post.description}
-				cover={singlePost.post.before_img}
-			/>
-			<Overlay as={Container}>
-				<Button confirm="true"
+}) => (
+	<Wrapper>
+		<SEO
+			url={`/post/${singlePost.post._id}`}
+			title={singlePost.post.title}
+			description={singlePost.post.description}
+			cover={singlePost.post.before_img}
+		/>
+		<Overlay as={Container}>
+			<Left>
+				<button
+					type="button"
 					onClick={() => {
 						if (history.action === 'PUSH') history.goBack()
 						else history.push('/')
 					}}
-				>Go back</Button>
-				<Post
-					{...singlePost.post}
-					userId={auth.user && auth.user._id}
-					isLoggedIn={auth.isLoggedIn}
-					userName={auth.user && auth.user.username}
-					voteBefore={voteBefore}
-					voteAfter={voteAfter}
-					postNewComment={postNewComment}
-					deleteComment={deleteComment}
-					deletePost={deletePost}
-					showComments
-				/>
-				{!auth.isLoggedIn && <Button confirm="true" as={Link} to={`/register/?from=${singlePost.post.title}`}>Sign Up to vote!</Button>}
-			</Overlay>
-		</Wrapper>
-	)
-}
+				>
+					<img src={backArrow} alt="Go Back" />
+				</button>
+			</Left>
+			<Post
+				{...singlePost.post}
+				userId={auth.user && auth.user._id}
+				isLoggedIn={auth.isLoggedIn}
+				userName={auth.user && auth.user.username}
+				voteBefore={voteBefore}
+				voteAfter={voteAfter}
+				postNewComment={postNewComment}
+				deleteComment={deleteComment}
+				deletePost={deletePost}
+				showComments
+			/>
+			{!auth.isLoggedIn && <Button confirm="true" as={Link} to={`/register/?from=${singlePost.post.title}`}>Sign Up to vote!</Button>}
+		</Overlay>
+	</Wrapper>
+)
 
 const mapStateToProps = ({ singlePost, auth }) => ({
 	singlePost,
--- src/modules/post/styles.js
+++ src/modules/post/styles.js
@@ -10,9 +10,21 @@ export const Wrapper = styled.div`
 	background: rgba(0, 0, 0, 0.88);
 	display: flex;
 	justify-content: center;
+	z-index: 5;
 `
 
 export const Overlay = styled.div`
 	align-self: center;
 	text-align: center;
+`
+
+export const Left = styled.div`
+	text-align: left;
+	margin-bottom: 1rem;
+
+	button {
+		background: none;
+		border: none;
+		cursor: pointer;
+	}
 `
